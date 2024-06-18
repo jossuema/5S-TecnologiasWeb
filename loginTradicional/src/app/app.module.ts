1704619444015e69b539ef57cfdcfff16585d358
jossuema <mmalla1@outlook.com>
2024-06-18 11:15:11 -0500

    Firebase Login

---
+++ loginTradicional/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegistroComponent,
+    LoginComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SweetAlert2Module.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ loginTradicional/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "tu_api_key",
+      authDomain: "tu_auth_domain",
+      projectId: "tu_project_id",
+      storageBucket: "tu_storage_bucket",
+      messagingSenderId: "tu_messaging_sender_id",
+      appId: "tu_app_id",
+      measurementId: "tu_measurement_id"
+    }
+  };
+  
